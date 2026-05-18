3ee4aaac7a1b1cdb0f9b3bd931de392c8e06bb00
Oleksandr Yavorskyi <55851245+slimnation23@users.noreply.github.com>
2026-05-18 20:37:21 +0200

    Change target date in countdown script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('#container');
 const result = document.querySelector('#result');
-const targetDate = new Date('2026-07-12T07:00:00').getTime();
+const targetDate = new Date('2026-07-05T07:00:00').getTime();
 
 function updateCountdown() {
   const now = Date.now();
